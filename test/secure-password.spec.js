0b8c7f98d2984fd4525a19c59fb5599d6506524b
Matt Venables <mattvenables@gmail.com>
2017-02-20 14:34:04 -0500

    Only perform asynchronous, save-time hashing
    The original default method of hashing passwords during the `setter`
    method was not ideal for node async i/o. The benefit of doing it at
    set-time was to immediately hash the password, to prevent any leaking.
    To add some of this security to the asynchronous method, we use the
    bookshelf `virtuals` plugin, and never set `password` on the model
    `attributes` hash.  This will prevent the password from being exposed
    during a toJSON call (or other).

--- test/secure-password.spec.js
+++ test/secure-password.spec.js
@@ -10,173 +10,139 @@ const securePassword = require('../lib/secure-password.js')
 describe('bookshelf-secure-password', function () {
   let bookshelf
   let knex
+  let model
+  let BasicModel
+  let CustomModel
 
   before(function () {
-    knex = new Knex({
-      client: 'pg'
-    })
+    knex = new Knex({ client: 'pg' })
     mockKnex.mock(knex)
+
+    bookshelf = new Bookshelf(knex)
+    bookshelf.plugin(securePassword)
+
+    BasicModel = bookshelf.Model.extend({
+      hasSecurePassword: true
+    })
+
+    CustomModel = bookshelf.Model.extend({
+      hasSecurePassword: 'custom_column'
+    })
   })
 
   after(function () {
     mockKnex.unmock(knex)
   })
 
-  describe('synchronous behavior', function () {
-    before(function () {
-      bookshelf = new Bookshelf(knex)
-      bookshelf.plugin(securePassword)
-    })
-
+  describe('password hashing', function () {
     describe('with the default column', function () {
-      let model
-
-      before(function () {
-        const Model = bookshelf.Model.extend({
-          hasSecurePassword: true
-        })
-
-        model = new Model({ password: 'testing' })
+      beforeEach(function () {
+        model = new BasicModel({ id: 1, password: 'testing' })
       })
 
-      it('does not keep the raw password on the model', function () {
+      describe('before save', function () {
+        it('does not keep the raw password on the model', function () {
+          expect(model.get('password')).to.be.undefined
+          expect(model.attributes.password).to.be.undefined
+
+          expect(model.get('password_digest')).to.be.undefined
+          expect(model.attributes.password_digest).to.be.undefined
+        })
+      })
+
+      describe('after save', function () {
+        beforeEach(function () {
+          return model.save()
+        })
+
+        it('sets the password digest field to null if given a `null` value', function () {
+          expect(model.get('password_digest')).to.be.a.string
+          model.set('password', null)
+
+          return model.save().then(() => {
+            expect(model.get('password_digest')).to.be.null
+          })
+        })
+
+        it('does not change the password digest if given undefined', function () {
+          let originalString = model.get('password_digest')
+          model.set('password', undefined)
+
+          return model.save().then(() => {
+            expect(model.get('password_digest')).to.equal(originalString)
+          })
+        })
+
+        it('does not change the password digest if given an empty string', function () {
+          let originalString = model.get('password_digest')
+          model.set('password', '')
+
+          return model.save().then(() => {
+            expect(model.get('password_digest')).to.equal(originalString)
+          })
+        })
+
+        it('changes the password digest if given a blank (spaces-only) string', function () {
+          let originalString = model.get('password_digest')
+          model.set('password', '  ')
+          return model.save().then(() => {
+            expect(model.get('password_digest')).to.be.a.string
+            expect(model.get('password_digest')).not.to.equal(originalString)
+          })
+        })
+      })
+
+      it('handles the case if a later validation throws an exception', function () {
+        let digest
+
+        model.on('saving', function (model) {
+          throw new Error()
+        })
+
+        return model
+          .save()
+          .then(() => {
+            expect(false).to.be.true
+          }, () => {
+            expect(model.get('password')).to.be.undefined
+            expect(model.get('password_digest')).to.be.a.string
+            digest = model.get('password_digest')
+            return model.save()
+          })
+          .then(() => {
+            expect(false).to.be.true
+          }, () => {
+            expect(model.get('password_digest')).to.equal(digest)
+          })
+      })
+    })
+
+    describe('with a custom column', function () {
+      before(function () {
+        model = new CustomModel({ id: 2, password: 'testing' })
+        return model.save()
+      })
+
+      it('allows the default column to be overwritten', function () {
         expect(model.get('password')).to.be.undefined
         expect(model.attributes.password).to.be.undefined
 
-        expect(model.get('password_digest')).to.be.a.string
-        expect(model.attributes.password_digest).to.be.a.string
+        expect(model.get('custom_column')).to.be.a.string
+        expect(model.attributes.custom_column).to.be.a.string
       })
-
-      it('sets the password digest field to null if given a `null` value', function () {
-        expect(model.get('password_digest')).to.be.a.string
-        model.set('password', null)
-        expect(model.get('password_digest')).to.be.null
-      })
-
-      it('does not change the password digest if given undefined', function () {
-        let originalString = model.get('password_digest')
-        model.set('password', undefined)
-        expect(model.get('password_digest')).to.equal(originalString)
-      })
-
-      it('does not change the password digest if given an empty string', function () {
-        let originalString = model.get('password_digest')
-        model.set('password', '')
-        expect(model.get('password_digest')).to.equal(originalString)
-      })
-
-      it('changes the password digest if given a blank (spaces-only) string', function () {
-        let originalString = model.get('password_digest')
-        model.set('password', '  ')
-        expect(model.get('password_digest')).to.be.a.string
-        expect(model.get('password_digest')).not.to.equal(originalString)
-      })
-    })
-
-    it('allows the default column to be overwritten', function () {
-      const Model = bookshelf.Model.extend({
-        hasSecurePassword: 'custom_column'
-      })
-
-      let model = new Model({ password: 'testing' })
-      expect(model.get('password')).to.be.undefined
-      expect(model.attributes.password).to.be.undefined
-
-      expect(model.get('custom_column')).to.be.a.string
-      expect(model.attributes.custom_column).to.be.a.string
-    })
-  })
-
-  describe('asynchronous save-time behavior', function () {
-    let model
-
-    before(function () {
-      bookshelf = new Bookshelf(knex)
-      bookshelf.plugin(securePassword, {
-        performOnSave: true
-      })
-
-      const Model = bookshelf.Model.extend({
-        hasSecurePassword: true
-      })
-
-      model = new Model({ id: 1, password: 'testing' })
-
-      expect(model.get('password')).to.equal('testing')
-      expect(model.get('password_digest')).to.be.undefined
-    })
-
-    it('saves the hashed password, clearing the raw password field', function () {
-      return model.save().then((model) => {
-        expect(model.get('password')).to.be.undefined
-        expect(model.get('password_digest')).to.be.a.string
-      })
-    })
-
-    it('handles the case if a later validation throws an exception', function () {
-      let digest
-
-      model.on('saving', function (model) {
-        throw new Error()
-      })
-
-      return model
-        .save()
-        .then(() => {
-          expect(false).to.be.true
-        }, () => {
-          expect(model.get('password')).to.be.undefined
-          expect(model.get('password_digest')).to.be.a.string
-          digest = model.get('password_digest')
-          return model.save()
-        })
-        .then(() => {
-          expect(false).to.be.true
-        }, () => {
-          expect(model.get('password_digest')).to.equal(digest)
-        })
     })
   })
 
   describe('#authenticate', function () {
-    let model
-
-    describe('synchronous behavior', function () {
-      before(function () {
-        bookshelf = new Bookshelf(knex)
-        bookshelf.plugin(securePassword)
+    describe('with hasSecurePassword enabled on the model', function () {
+      beforeEach(function () {
+        model = new BasicModel({ id: 1, password: 'testing' })
       })
 
-      describe('with hasSecurePassword enabled on the model', function () {
-        before(function () {
-          const Model = bookshelf.Model.extend({
-            hasSecurePassword: true
-          })
-
-          model = new Model({ password: 'testing' })
-        })
-
-        it('resolves the Model if the password matches', function () {
+      describe('before save', function () {
+        it('does not authenticate until the record is saved', function () {
           return model.authenticate('testing').then((model) => {
-            expect(model).to.be.defined
-          }, (err) => {
-            expect(err).to.be.undefined
-          })
-        })
-
-        it('rejects with a PasswordMismatchError if the password does not match', function () {
-          return model.authenticate('invalid').then((model) => {
-            expect(model).to.be.defined
-          }, (err) => {
-            expect(err).to.be.defined
-            expect(err).to.be.an.instanceof(PasswordMismatchError)
-            expect(err.name).to.equal('PasswordMismatchError')
-          })
-        })
-
-        it('rejects with a PasswordMismatchError if the no password is provided', function () {
-          return model.authenticate().then((model) => {
-            expect(model).to.be.defined
+            expect(false).to.be.true
           }, (err) => {
             expect(err).to.be.defined
             expect(err).to.be.an.instanceof(PasswordMismatchError)
@@ -185,38 +151,8 @@ describe('bookshelf-secure-password', function () {
         })
       })
 
-      describe('without hasSecurePassword on this model', function () {
-        it('calls the model`s `authenticate` method', function () {
-          const Model = bookshelf.Model.extend({})
-          model = new Model({ password: 'testing' })
-
-          try {
-            return model.authenticate('testing')
-          } catch (err) {
-            expect(err).to.be.defined
-            expect(err).to.be.an.instanceof(TypeError)
-          }
-        })
-      })
-    })
-
-    describe('asynchronous save-time behavior', function () {
-      let model
-
-      before(function () {
-        bookshelf = new Bookshelf(knex)
-        bookshelf.plugin(securePassword, {
-          performOnSave: true
-        })
-      })
-
-      describe('with hasSecurePassword enabled on the model', function () {
-        before(function () {
-          const Model = bookshelf.Model.extend({
-            hasSecurePassword: true
-          })
-
-          model = new Model({ id: 1, password: 'testing' })
+      describe('after save', function () {
+        beforeEach(function () {
           return model.save()
         })
 
@@ -230,7 +166,7 @@ describe('bookshelf-secure-password', function () {
 
         it('rejects with a PasswordMismatchError if the password does not match', function () {
           return model.authenticate('invalid').then((model) => {
-            expect(model).to.be.defined
+            expect(false).to.be.true
           }, (err) => {
             expect(err).to.be.defined
             expect(err).to.be.an.instanceof(PasswordMismatchError)
@@ -248,22 +184,19 @@ describe('bookshelf-secure-password', function () {
           })
         })
       })
+    })
 
-      describe('without hasSecurePassword on this model', function () {
-        it('calls the model`s `authenticate` method', function () {
-          const Model = bookshelf.Model.extend({})
-          model = new Model({ id: 1, password: 'testing' })
+    describe('without hasSecurePassword on this model', function () {
+      it('calls the model`s `authenticate` method', function () {
+        const Model = bookshelf.Model.extend({})
+        model = new Model({ id: 1, password: 'testing' })
 
-          return model
-            .save()
-            .then((model) => {
-              model.authenticate('testing')
-            })
-            .catch((err) => {
-              expect(err).to.be.defined
-              expect(err).to.be.an.instanceof(TypeError)
-            })
-        })
+        try {
+          return model.authenticate('testing')
+        } catch (err) {
+          expect(err).to.be.defined
+          expect(err).to.be.an.instanceof(TypeError)
+        }
       })
     })
   })
